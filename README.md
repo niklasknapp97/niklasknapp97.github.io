dd19f9ec2c299cda765daeccec9f6a0b5c24f826
niklasknapp97 <71630022+niklasknapp97@users.noreply.github.com>
2024-09-18 20:59:18 +0000

    Process data done

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# niklasknapp97.github.io
+![Cinema](img/title_image.png)
+
+# Data Analysis of AirBnB reviews
+
+This document outlines the analysis performed on a DataFrame with reviews on Airbnb listings. It contains the following columns: `listing_id`, `id`, `date`, `reviewer_id`, `reviewer_name`, `comments`. The analyses include visualizations to help understand the data better.
+
+## Motivation
+
+The goal of this report is to create an understanding for the the reviews of Airbnb listings.
+
+## View the Analysis
+
+[View Analysis Report](analysis.md)
+
+## Libraries used
+
+* Pandas
+* Matplotlib
+* Seaborn
+* Wordcloud
+
+## Files
+
+* analysis_files/
+  * exported pictures of the diagrams
+* data/
+  * reviews.csv: The CSV-file containing the data
+* img/
+  * images
+* analysis.jpynb: The Jupyter notebook containing the code
+* anaysis.md: The markdown file exported from the jupyter notebook
+* reviews.csv:
+
+
+## Data Analysis
+
+### 1. Distribution of Reviews by Listing
+
+**Objective:** Visualize the number of reviews each listing has received.
+
+**Diagram:**
+
+![png](analysis_files/analysis_9_0.png)
+
+**Findings:** This gives a good insight how many reviews there are per listing.
+
+### 2. Distribution of Reviews over time
+
+**Objective:** Visualize the number of reviews over time
+
+**Diagram:**
+
+![png](analysis_files/analysis_11_0.png)
+
+**Finding:** The number of reviews has increased drastically over the last years and there is a peak of reviews towards the end of each year.
+
+### 3. Review Counts by Reviewer
+
+**Objective:** Determine the distribution of the number of reviews written by each of the top ten reviewers.
+
+**Diagram:**
+
+![png](analysis_files/analysis_13_0.png)
+
+**Finding:** The top reviewer has more than double the reviews of the number ten reviewer.
+
+### 4. Temporal Distribution of Reviews
+
+**Objective:** Analyze the distribution of reviews over time.
+
+**Diagram:**
+
+![png](analysis_files/analysis_15_0.png)
+
+**Finding:** The by far most listings have very few reviews, only a few listings have lots of reviews.
+
+### 5. Common Words in Comments
+
+**Objective:** Identify the most frequently occurring words in the comments.
+
+**Diagram:**
+
+![png](analysis_files/analysis_17_0.png)
+
+**Findings:** The most often used words don't have anything in common except that they are related to travel, which is kind of expected. The most often visited city in this dataset seems to be Seattle.
+
+## View the Analysis
+
+[View Analysis Report](analysis.md)
+
+## Conclusion
+
+These analyses provide insights into the distribution and characteristics of the reviews dataset. Use the diagrams to gain a deeper understanding of review patterns and trends.
---
+++ data/process_data.py
@@ -0,0 +1,88 @@
+# import libraries
+import pandas as pd
+from sqlalchemy import create_engine
+import sys
+
+
+def load_data(messages_filepath, categories_filepath):
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = pd.merge(messages, categories, on='id', how='inner')
+
+    return df
+
+
+def clean_data(df):
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # extract a list of new column names for categories
+    category_colnames = row.apply(lambda x: x.split('-')[0])
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.split('-').str[1].astype(int)
+
+        # convert column from string to numeric
+        categories[column] = categories[column].apply(lambda x: 1 if x > 1 else x)
+    
+    # drop the original categories column from `df`
+    df.drop('categories', axis=1, inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    return df
+
+
+def save_data(df, database_filename):
+
+    # create sql alchemy engine
+    engine = create_engine(f'sqlite:///{database_filename}')
+
+    # write data to database
+    df.to_sql('messages', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
